00d202f6abecc580619a44a5175c8dc5d5f6f5e3
murata-yuka <murata-yuka@9105-109520.local>
2017-01-26 22:00:49 +0900

    コードのインデント修正

--- modal.js
+++ modal.js
@@ -1,19 +1,19 @@
-$(function(){
+$(function () {
 
 // モーダルウィンドウが開くときの処理	
-$(".modal_1").click(function(){
-	$("#modal01").fadeIn();
-	$(".overlay").fadeIn();
-	$(this).addClass("open");
-	return false;
-});
+    $(".modal_1").click(function () {
+        $("#modal01").fadeIn();
+        $(".overlay").fadeIn();
+        $(this).addClass("open");
+        return false;
+    });
 
 // モーダルウィンドウが閉じるときの処理	
-$(".modalclose").click(function(){
-	$("#modal01").fadeOut();
-	$(".overlay").fadeOut();
-	return false;
-});  
+    $(".modalclose").click(function () {
+        $("#modal01").fadeOut();
+        $(".overlay").fadeOut();
+        return false;
+    });
 
 
 });
