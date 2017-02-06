6f7336286efc518ff8fc2b46d2903dff83d05076
murata-yuka <murata-yuka@9103-147346.ad.imjp.co.jp>
2017-02-06 11:22:30 +0900

    ボタンに応じたモーダルの出力

--- js/modal.js
+++ js/modal.js
@@ -1,33 +1,34 @@
 $(function () {
-	var modaldata;
-	getdata();
-	closemodal();
+    var modaldata;
+    getdata();
+    closemodal();
 
 
-	function getdata (){
-		$('.button').click(function(){
-			modaldata = $(this).attr('data-modal');
-			openmodal(modaldata);
-		});
-	}
+    function getdata() {
+        $('.button').click(function () {
+            modaldata = $(this).attr('data-modal');
+            openmodal(modaldata);
+        });
+    }
 
-	function openmodal (){
-		$('.modal[data-modal=modal01], .overlay').velocity('fadeIn',{
-			duration:350,
-			complete: function(){
+    function openmodal() {
+            $('.modal[data-modal="'+ modaldata + '"], .overlay').velocity('fadeIn', {
+                duration: 350,
+                complete: function () {
 
-			}
-		});
-	}
-	function closemodal (){
-		$('.modalclose').click(function(){
-			$('.modal[data-modal=modal01], .overlay').velocity('fadeOut',{
-				duration:350,
-				complete:function(){
+                }
+            });
+    }
 
-				}
-			});
-		});
-	}
+    function closemodal() {
+        $('.modalclose').click(function () {
+            $('.modal[data-modal="'+ modaldata + '"], .overlay').velocity('fadeOut', {
+                duration: 350,
+                complete: function () {
+
+                }
+            });
+        });
+    }
 
 });
